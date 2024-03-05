becac1b7b31f7b6eb1c6ce0e03516558e370f1f5
Yan Sharma <yawning008@gmail.com>
2024-03-05 10:19:44 -0500

    ad

--- api/src/views/HomeView.vue
+++ api/src/views/HomeView.vue
@@ -1,8 +1,24 @@
 <script setup>
+async function today(x) {
+  try {
+    const response = await fetch("https://data.cityofnewyork.us/resource/43nn-pn8j.json?$limit=300000");
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+
+    const data = await response.json();
+    console.log(data); 
+
+  } catch(err) {
+    console.log(err.message); 
+  }
+
+}
+today()
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>hey</h1>
   </main>
 </template>
