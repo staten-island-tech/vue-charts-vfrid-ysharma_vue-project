0233dc51f1f9f4f5bdd2478a94e10ab80704a745
Vfrid <113192881+Vfrid@users.noreply.github.com>
2024-03-06 09:37:24 -0500

    fdassd

--- api/src/App.vue
+++ api/src/App.vue
@@ -1,10 +1,17 @@
-<script setup>
+<script>
 import { RouterLink, RouterView } from 'vue-router'
+import BarChart from './components/BarChart.vue'
+
+export default {
+  name: 'App',
+  components: { BarChart }
+}
+
 </script>
 
 <template>
 
-
+<BarChart />
   <RouterView />
 </template>
 
