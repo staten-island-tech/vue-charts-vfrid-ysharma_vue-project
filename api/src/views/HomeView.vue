e7baca6c857b9799a96eb46142c8ed553041f864
Yan Sharma <yawning008@gmail.com>
2024-03-07 10:13:04 -0500

    azwds

--- api/src/views/HomeView.vue
+++ api/src/views/HomeView.vue
@@ -1,4 +1,10 @@
-<script setup>
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+
 async function today(x) {
   try {
     const response = await fetch("https://data.cityofnewyork.us/resource/43nn-pn8j.json?$limit=300000");
@@ -8,17 +14,63 @@ async function today(x) {
 
     const data = await response.json();
     console.log(data); 
+    return data
 
   } catch(err) {
     console.log(err.message); 
   }
-
 }
-today()
+let boros = []
+let boronumbers=[]
+let violations = []
+let violationnumbers = []
+let criticalflag = []
+let criticalnumber = []
+
+async function f1() {
+  const data = await today()
+  for(let i=0; i<data.length; i++){
+    if(data[i].boro!=="0" && !boros.includes(data[i].boro)){
+      boros.push(data[i].boro)
+      boronumbers.push(0)
+    }
+    else{
+      let index=boros.indexOf(data[i].boro)
+      boronumbers[index]+=1
+    }
+  }
+  let returned = {
+        labels: boros,
+        datasets: [ { data: boronumbers } ]
+      }
+  return returned
+}
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  data: () => ({
+    loaded: false,
+    chartData: null
+  }),
+  async mounted () {
+    this.loaded = false
+
+    try {
+      const { userlist } = await f1()
+      this.chartdata = userlist
+
+      this.loaded = true
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
 </script>
 
 <template>
   <main>
-    <h1>hey</h1>
+    <Bar v-if="loaded" :data="chartData" />
+  <button @click="rask">boros</button>
   </main>
 </template>
